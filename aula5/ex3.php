1de0c26ca14719068fad42e83f834e93db949536
Gabrielle <gabrielle.stabile@aluno.edu.ifsp.br>
2022-03-23 11:04:46 -0300

    ALTERAÇÃO TRÊS

---
+++ aula5/ex3.php
@@ -0,0 +1,11 @@
+<?php
+
+    $opcao = 'a';
+
+    switch($op){
+        case 'a': echo "ABRIR"; break;
+        case 'a': echo "FECHAR"; break;
+        case 'a': echo "SALVAR"; break;
+        default: echo "SAIR"; break;
+    }
+?>
