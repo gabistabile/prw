57bf5953a69d236a42ccb3dad9205b2568a31da9
Gabrielle <gabrielle.stabile@aluno.edu.ifsp.br>
2022-03-23 10:54:01 -0300

    ALTERAÇÃO DOIS

---
+++ aula5/ex2.php
@@ -0,0 +1,14 @@
+<?php
+
+    $N1 = 5;
+    $N2 = 3;
+    $media = ($N1 + $N2)/2;
+
+    if($media >= 7)
+
+        echo "VOCÊ FOI APROVADO";
+    
+    else
+        echo "VOCÊ FOI REPROVADO";
+
+?>
