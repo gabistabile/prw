bae353cd9d2d096b0303644c8b59186ca191f63b
Gabrielle <gabrielle.stabile@aluno.edu.ifsp.br>
2022-03-23 11:44:30 -0300

    exercicio 1 corrigido

--- aula5/exercicio1.php
+++ aula5/exercicio1.php
@@ -1,10 +1,23 @@
 <?php
 
     $N1 = 12;
-    $N2 = 44;
+    $N2 = 7;
     $N3 = 8;
+   
+    if($N2 > $N1 && $N1 > $N3)
+            echo "ORDEM DECRESCENTE: ".$N2.$N1.$N3;
+    else
+        if ($N2 > $N3 && $N3 > $N1)
+            echo "ORDEM DECRESCENTE: ".$N2.$N3.$N1;
 
-    if($N2 > $N1)
-        if($N1 > $N3)
-        echo "ORDEM DECRESCENTE: 44, 12, 8";
+        else
+            if ($N3 > $N1 && $N1 > $N2)
+            echo "ORDEM DECRESCENTE: ".$N3.$N1.$N2; 
+    echo "ORDEM DEC112621616RESCENTE: ";
+            //N2 > N1 --
+            //N2 > N3 --
+            //N1 > N2 --
+            //N1 > N3 --
+            //N3 > N1 --
+            //N3 > N2 --
 ?>
