1c6afae174561de41c586ea542e0112d3f12aa4a
gabistabile <gabrielle.stabile@aluno.ifsp.edu.br>
2022-03-30 10:54:24 -0300

    Formulario

---
+++ aula6/exString.php
@@ -0,0 +1,7 @@
+<?php
+        $palavra = 'Ciência da Computação';
+        echo 'A variável contém: ' .strlen($palavra) .'caracteres' .'<br>';
+        
+        $palavra = trim($palavra);
+        echo 'A variável agora contém: ' .strlen($palavra) . 'caracteres';
+?>
