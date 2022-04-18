11c3d1d91e747e51886d5f79b52c9202968cc39a
gabistabile <gabrielle.stabile@aluno.ifsp.edu.br>
2022-04-17 23:07:32 -0300

    formularioemphp

---
+++ Lista 5/formulario_ex3.php
@@ -0,0 +1,10 @@
+<?php
+/*um programa em PHP deverá ser capaz de 
+encontrar esse nome de usuário em um vetor e verificar se a senha digitada está de 
+acordo com a senha armazenada nesse vetor. Utilizar o índice do vetor como sendo o 
+login do usuário.*/ 
+
+        $login = $_GET['login']; //LOGIN DO USUÁRIO
+        $senha = $_GET['senha']; //SENHA DO USUÁRIO
+
+?>
